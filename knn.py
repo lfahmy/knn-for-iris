cf35336a3867497408e56aba410902b6efa6f626
lfahmy <lfahmy38@gmail.com>
2017-10-10 11:51:15 -0700

    cleaner knn class

--- knn.py
+++ knn.py
@@ -7,7 +7,6 @@ class Knn:
 	classes = [] #the list of all possible classes
 	classFrame = DataFrame() #frame of distance to the test point and corresponding classes
 	classDict = {} #keeps track of frequency of each class
-	K = 10 #THIS IS A CONSTANT representing k
 
 	def __init__(self, labeled, unlabeled, classes):
 		self.labeled = labeled
@@ -31,9 +30,9 @@ class Knn:
 			newSeries = point.subtract(series)
 			#map applies a function to all elements in a series.
 			#here we square each element, preparing to get the final distance
-			newSeries = newSeries.map(lambda x: x**2)
+			newSeries = newSeries.map(lambda x: x**100)
 			toRoot = newSeries.sum() #the number to root
-			final = sqrt(toRoot) #the final distance from the labeled point to the unlabeled
+			final = toRoot**(.01) #the final distance from the labeled point to the unlabeled
 			distances.append(final)
 		#make a series from the list of distances with indices matching the classSeries
 		distanceSeries = Series(distances)
@@ -44,33 +43,28 @@ class Knn:
 		self.classFrame = concat([classSeries, distanceSeries], axis=1)
 		self.classFrame.columns = ['class', 'distances']
 
-	def classifyOne(self, point, index):
+	def classifyOne(self, point):
 		self.distance(point) #set up the classFrame for this method
 		self.classFrame = self.classFrame.sort_values('distances')
 
 		#take the first K classes of the sorted classFrame and count the
 		#frequency of each class
-		for index, row in self.classFrame.head(n=self.K).iterrows():
+		for index, row in self.classFrame.head(n=10).iterrows():
 			thisClass = row['class'] #class of current row
 			for type in self.classes:
 				#add 1 to the frequency (in classDict) of whichever class this is
 				if thisClass == type:
 					self.classDict[thisClass] += 1
 		#order the dict from highest to lowest frequency and return the highest
-		answer = None
 		for key in sorted(self.classDict, key=self.classDict.get, reverse=True):
-			if answer == None:
-				answer = key
-			print(str(index) + " " + key + " {0:.0f}%".format((float(self.classDict[key])/float(self.K))*100), end=" ")
-		print()
-		return answer
+			return key
 
 	def classifyAll(self):
 		answers = [] #list of classifications for each unlabeled point
 		#go through each unlabeled point and classify it using classifyOne()
 		for index, point in self.unlabeled.iterrows():
 			#add result of classification to list of answers
-			answers.append(self.classifyOne(point, index))
+			answers.append(self.classifyOne(point))
 			#reset frequency count of each class for the next point's classification
 			for key in self.classes:
 				self.classDict[key] = 0
@@ -85,12 +79,9 @@ class Knn:
 		correct = 0 #number of points we classified correctly
 		#go through our answers and increment "correct" each time we got
 		#a point right
-		print(response)
 		for index, row in response.iterrows():
 			tries += 1
 			if row['class'] == solution['class'][index]:
 				correct += 1
-			else:
-				print(str(index) + " Your answer: " + row['class'] + "   Correct answer: " + solution['class'][index])
-		print(str(correct) + " out of " + str(tries) + " correct answers")
-		print("{0:.0f}%".format((float(correct)/float(tries))*100) + " accuracy")
+		print str(correct) + " out of " + str(tries) + " correct answers"
+		print "{0:.0f}%".format((float(correct)/float(tries))*100) + " accuracy"
